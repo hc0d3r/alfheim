0cdfb0e133582c7929cd71ea220482748a8b989f
Matheus <mmedeiros450@gmail.com>
2019-05-22 00:43:19 -0300

    update command line options

--- src/main.c
+++ src/main.c
@@ -11,35 +11,42 @@
 
 void parser_args(int argc, char **argv, options_t *opt){
     static const struct option options[]={
-        {"sc-file",           required_argument, NULL, 'f'},
-        {"sc-string",         required_argument, NULL, 's'},
-        {"no-restore",        no_argument,       NULL, 'n'},
-        {"ptrace",            no_argument,       NULL, 'p'},
-        {"no-restore-ip",        no_argument,       NULL, 'N'},
+        {"shellcode",         required_argument, NULL, 's'},
+        {"file",              required_argument, NULL, 'f'},
+        {"no-restore-memory", no_argument,       NULL,   0},
+        {"no-restore-ip",     no_argument,       NULL,   0},
+        {"ptrace",            no_argument,       NULL,   0},
         {NULL, 0, NULL, 0}
     };
 
     int index = 0, optc;
+    const char *name;
 
-    while((optc = getopt_long(argc, argv, "f:s:npN", options, &index)) != -1){
+    while((optc = getopt_long(argc, argv, "s:f:", options, &index)) != -1){
         switch(optc){
-            case 'f':
-                opt->filename = optarg;
+            case 0:
+                name = options[index].name;
+                if(!strcmp(name, "no-restore-memory")){
+                    opt->options.restore = 0;
+                }
+
+                else if(!strcmp(name, "no-restore-ip")){
+                    opt->options.restore_ip = 0;
+                }
+
+                else if(!strcmp(name, "ptrace")){
+                    memwrite = ignotum_ptrace_write;
+                    memread = ignotum_ptrace_read;
+                }
+
                 break;
 
             case 's':
                 opt->shellcode = optarg;
                 break;
 
-            case 'n':
-                opt->options.restore = 0;
-                break;
-            case 'p':
-                memwrite = ignotum_ptrace_write;
-                memread = ignotum_ptrace_read;
-                break;
-            case 'N':
-                opt->options.restore_ip = 0;
+            case 'f':
+                opt->filename = optarg;
                 break;
 
             case '?':
@@ -60,11 +67,15 @@ void parser_args(int argc, char **argv, options_t *opt){
 void help(void){
     static const char help_menu[]=
         "Usage: alfheim [OPTIONS] [PID]\n\n"
-        "   -f, --sc-file FILE       File contains shellcode bytes\n"
-        "   -s, --sc-string STRING   Shellcode string, e.g '\\x90\\x90\\x90'\n"
-        "   -n, --no-restore         No restore memory overwrited by shellcode\n"
-        "   -N, --no-restore-ip      No restore instruction point\n"
-        "   -p, --ptrace             Inject code using ptrace, instead of write in /proc/[pid]/mem\n";
+        "Options:\n"
+        "  -s, --shellcode STRING   string with shellcode, e.g, '90 90 90',\n"
+        "                            '0x90, 0x90', '\\x90\\x90\\x90'\n"
+        "  -f, --file FILE          file with shellcode\n\n"
+
+        "  --no-restore-memory      no restore memory after shellcode execution\n"
+        "  --no-restore-ip          no restore instruction point after shellcode execution\n\n"
+
+        "  --ptrace                 write/read the memory using ptrace instead of /proc/[pid]/mem";
 
     puts(help_menu);
     exit(0);
